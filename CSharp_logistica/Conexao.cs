45e0584bbacc1c6ba5082ec81fcd0b2afe3fa98f
HIGOR HENRY DE LIMA E SOUZA <higor.hlesouza@senacsp.edu.br>
2025-09-29 17:50:10 -0300

    2

---
+++ CSharp_logistica/Conexao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_logistica
+{
+   public static class Conexao
+    {
+
+
+        public static string stringConexao = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Logistica;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+
+}
